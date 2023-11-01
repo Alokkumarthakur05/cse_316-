c27489f8f208a1d75bc06847d1a0b451aeef07b3
Alok <149518563+Alokkumarthakur05@users.noreply.github.com>
2023-11-01 20:49:49 +0530

    Add files via upload

---
+++ Project.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	int i, type[20],n;
+	int responsetime[20];
+	printf("Number of process: ");
+	scanf("%d",&n);
+	printf("Enter the response time for various processes:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("Response time of P%d (in milliseconds): ",i);
+		scanf("%d",&responsetime[i]);
+		if(responsetime[i]<1000)
+		{
+			type[i]=1;
+		}
+		else
+		{
+			type[i]=0;
+		}
+	}
+	printf("Process Number\tResponse Time\tType\t\tPriority");
+	printf("\n=========================================================");
+	for(i=0;i<n;i++)
+	{
+		printf("\nP%d\t\t%dms\t\t",i,responsetime[i]);
+		if(type[i]==1)
+		{
+			printf("Interactive\tHigh");
+		}
+		else
+		{
+			printf("Non-Interactive\tLow");
+		}
+	}
+}
